7040e35686ec15d622cb58fd2f4aa6f020baaea6
David Basil <info@basildavid.com>
2019-05-12 15:15:40 +0400

    edit handle_form.php again

--- ch02/handle_form.php
+++ ch02/handle_form.php
@@ -1,43 +1,72 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width" />
-	<title>Form Feedback</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width" />
+<title>Form Feedback</title>
+<style type="text/css" media="screen">
+.error {
+font-weight: bold;
+color: #C00;
+}
+</style>
 </head>
 <body>
 <?php 
-# script 2.2 - handle_form.php
+# script 2.4 - hand_form.php
 
-// get the form data
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$comments = $_REQUEST['comments'];
-
-// create the gender variable
-if (isset($_REQUEST['gender'])){
-	$gender = $_REQUEST['gender'];
+// validate the name
+if(!empty($_REQUEST['name'])){
+	$name = $_REQUEST['name'];
 } else {
-	$gender = NULL;
+	$name = NULL;
+	echo '<p class="error">You forgot to enter your name</p>';
 }
 
-// print the submitted info
-echo "<p>Thank you, 
-<strong>$name</strong>,
-for the following comments:
-</p>
-<pre>$comments</pre>
-<p>We will reply to you at <em>$email</em></p>";
-
-// print the message based upon the gender value
-if($gender == 'M'){
-	echo '<p><strong>Good day, Sir!</strong></p>';
-} elseif($gender == 'F'){
-	echo '<p><strong>Good day, Madam!</strong></p>';
+// validate the email
+if(!empty($_REQUEST['email'])){
+	$email = $_REQUEST['email'];
 } else {
-	echo '<p><strong>You forgot to enter gender!</strong></p>';
+	$email = NULL;
+	echo '<p class="error">You forgot to enter your email</p>';
+}
+
+// validate the comments
+if(!empty($_REQUEST['comments'])){
+	$comments = $_REQUEST['comments'];
+} else {
+	$comments = NULL;
+	echo '<p class="error">Your forgot to enter comments</p>';
+}
+
+// validate the gender
+if(isset($_REQUEST['gender'])){
+	$gender = $_REQUEST['gender'];
+	if($gender == 'M'){
+		$greeting = '<p><strong>Good day, Sir!</strong></p>';
+	} elseif($gender == 'F'){
+		$greeting = '<p><strong>Good day, Madam!</strong></p>';
+	} else {
+		$gender = NULL;
+		echo '<p class="error">Gender should be either "M" or "F"</p>';
+	}
+} else {
+	$gender = NULL;
+	echo '<p class="error">You forgot to select your gender</p>';
+}
+
+// if everythin is ok, print the message
+if($name && $email && $gender && $comments){
+	echo "<p>Thank you, <strong>$name</strong>, for the following comments:</p>
+	<pre>$comments</pre>
+	<p>We will reply to you at <em>$email</em></p>\n";
+	echo $greeting;
+} else {
+	echo '<p class="error">Please fill out the form again</p>';
 }
 
 ?>
+
+
 </body>
 </html>
