671d0543d0d477201f3522007daa9749564f448c
David Basil <info@basildavid.com>
2019-05-13 20:07:50 +0400

    add index.php

---
+++ ch03/index.php
@@ -0,0 +1,17 @@
+<?php 
+# script 3.4 - index.php
+$page_title = 'Welcom to this Site!';
+include('includes/header.html');
+?>
+
+<div class="page-header">
+	<h1>Content Header</h1>
+</div>
+<p>This is where the page-specific content goes. This section, and the corresponding header, will change from one page to the next.</p>
+<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+	tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+	vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+	no sea takimata sanctus est Lorem ipsum dolor sit amet.
+</p>
+
+<?php include('includes/footer.html'); ?>
