76ecbf080e7a4f356d35f445d43ddf17cbbfffbd
David Basil <info@basildavid.com>
2019-05-22 19:00:24 +0400

    add display_errors.php

---
+++ ch08/display_errors.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Display_errors</title>
+</head>
+<body>
+
+<h2>Testing display errors</h2>
+<?php # script 8.1 - display_errors.php
+
+// set errors reporting
+ini_set('display_errors', 1);
+
+// create errors
+foreach($var as $v){
+}
+$result = 1/0;
+
+
+?>
+
+</body>
+</html>
