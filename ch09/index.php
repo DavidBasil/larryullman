ab7c1f7bb8e6a0af902d17ff01b23fa1ff28936e
David Basil <info@basildavid.com>
2019-05-22 20:11:28 +0400

    add chapter 9

---
+++ ch09/index.php
@@ -0,0 +1,32 @@
+<?php 
+# script 3.4 - index.php
+
+// this function creates ads
+function create_ad(){
+	echo '<div class="alert alert-info" role="alert">
+		<p>This is annoying ad! This is annoying ad! This is annoying ad!</p>
+	</div>';
+} // end of function
+
+$page_title = 'Welcom to this Site!';
+include('includes/header.html');
+
+// call ads function
+create_ad();
+?>
+
+<div class="page-header">
+	<h1>Content Header</h1>
+</div>
+<p>This is where the page-specific content goes. This section, and the corresponding header, will change from one page to the next.</p>
+<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+	tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+	vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+	no sea takimata sanctus est Lorem ipsum dolor sit amet.
+</p>
+
+<?php 
+// call the ads function
+create_ad();
+include('includes/footer.html'); 
+?>
