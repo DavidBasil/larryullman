77214c3bc73a9fe6df733d605c351eacc4c5403c
David Basil <info@basildavid.com>
2019-05-26 14:10:07 +0400

    add upload_image.php

---
+++ ch11/upload_image.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title>Upload_image</title>
+<style type="text/css" media="screen">
+.error {
+	font-weight: bold;
+	color: #C00;
+}
+</style>
+</head>
+<body>
+<?php 
+// check if the form has been submitted
+
+
+?>
+</body>
+</html>
