dfe6ba9a35219efdd94d3a79d93852413598c4fe
David Basil <info@basildavid.com>
2019-05-26 15:46:35 +0400

    add function.js and images.php

---
+++ ch11/images.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Images</title>
+	<script charset="utf-8" src="js/function.js"></script>
+</head>
+<body>
+
+<p>Click on an image to view it in a separate window.</p>
+<ul>
+
+<?php  
+// this script lists the images in the uploads directory
+$dir = '../../../uploads';
+$files = scandir($dir);
+
+foreach($files as $image){
+	if(substr($image, 0, 1) != '.'){
+		$image_size = getimagesize("$dir/$image");
+		$image_name = urlencode($image);
+		echo "<li><a href=\"javascript:create_window('$image_name', $image_size[0], $image_size[1])\">$image</a></li>\n";
+	}
+}
+
+?>
+</ul>
+
+</body>
+</html>
---
+++ ch11/js/function.js
@@ -0,0 +1,14 @@
+// pop up window function
+function create_window(image, width, height){
+	width = width + 10;
+	height = height + 10;
+	if(window.popup && !window.popup.closed){
+		window.popup.resizeTo(width, height);
+	}
+	var specs = "location=no, scrollbars=no, menubar=no, toolbar=no, resizable=yes, left=0, top=0, width=" 
+		+ width + ", height=" 
+		+ height;
+	var url = "show_image.php?image=" + image;
+	popup = window.open(url, "ImageWindow", specs);
+	popup.focus();
+}
