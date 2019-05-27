9236d3619c869e242fe5389e8c72eed9110bfbe7
David Basil <info@basildavid.com>
2019-05-27 21:22:31 +0400

    add login.php

--- ch12/login.php
+++ ch12/login.php
@@ -1,5 +1,21 @@
 <?php 
 // login processing
 
-
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	require('includes/login_functions.inc.php');
+	require('../mysqli_connect.php');
+	// check the login
+	list($check, $data) = check_login($dbc, $_POST['email'], $_POST['pass']);
+	if($check){
+		// set cookies
+		setcookie('user_id', $data['user_id']);
+		setcookie('first_name', $data['first_name']);
+		// redirect
+		redirect_user('loggedin.php');
+	} else {
+		$errors = $data;
+	}
+	mysqli_close($dbc);
+}
+include('includes/login_page.inc.php');
 ?>
