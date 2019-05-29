e140f5cba6a05a33b637f17ac2bd5a3882f9a4a2
David Basil <info@basildavid.com>
2019-05-29 19:41:12 +0400

    edit logout.php

--- ch12/logout.php
+++ ch12/logout.php
@@ -1,20 +1,25 @@
 <?php 
-
+session_start();
 // if no cookie is present, redirect the user
-if(!isset($_COOKIE['user_id'])){
+if(!isset($_SESSION['user_id'])){
 	require('includes/login_functions.inc.php');
 	redirect_user();
 } else {
+	// delete session
+	$_SESSION = [];
+	session_destroy();	
+	// destroy cookie
+	setcookie('PHPSESSID', '', time()-3600, '/', 0, 0);
 	// delete cookie
-	setcookie('user_id', '', time()-3600, '/', '', 0, 0);
-	setcookie('first_name', '', time()-3600, '/', '', 0, 0);
+	/* setcookie('user_id', '', time()-3600, '/', '', 0, 0); */
+	/* setcookie('first_name', '', time()-3600, '/', '', 0, 0); */
 }
 
 $page_title = 'Logged out';
 include('includes/header.html');
 
 echo "<h2>Logged out</h2>
-	<p>{$_COOKIE['first_name']}, you are now logged out!</p>";
+	<p>You are now logged out</p>";
 
 include('includes/footer.html');
 
