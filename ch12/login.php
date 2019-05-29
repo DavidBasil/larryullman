6519856f1712a7e94c21791d43a1d8b61c134df8
David Basil <info@basildavid.com>
2019-05-29 18:56:46 +0400

    edit login.php to use sessions

--- ch12/login.php
+++ ch12/login.php
@@ -7,9 +7,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	// check the login
 	list($check, $data) = check_login($dbc, $_POST['email'], $_POST['pass']);
 	if($check){
+		// set session data
+		session_start();
+		$_SESSION['user_d'] = $data['user_id'];
+		$_SESSION['first_name'] = $data['first_name'];
 		// set cookies
-		setcookie('user_id', $data['user_id'], time()+3600, '/', '', 0, 0);
-		setcookie('first_name', $data['first_name'], time()+3600, '/', '', 0, 0);
+		/* setcookie('user_id', $data['user_id'], time()+3600, '/', '', 0, 0); */
+		/* setcookie('first_name', $data['first_name'], time()+3600, '/', '', 0, 0); */
 		// redirect
 		redirect_user('loggedin.php');
 	} else {
