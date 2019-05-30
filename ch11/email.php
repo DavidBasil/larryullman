c064c51c3f7ea2a0bb5a07b5bbfd554574b1bc7f
David Basil <info@basildavid.com>
2019-05-30 16:33:39 +0400

    edit mail.php

--- ch11/email.php
+++ ch11/email.php
@@ -12,21 +12,32 @@
 <?php 
 // check for form submission
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-	// form validation
-	if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['comments']))	{
-		// create the body
-		$body = "Name: {$_POST['name']}\n\nComments: {$_POST['comments']}";
-		// no longer than 70 characters
-		$body = wordwrap($body, 70);
-		// send the email
-		mail('your_example@example.com', 'Contact form submission', $body, "From: {$_POST['email']}");
-		// print the message
-		echo '<p><em>Thank you for contacting me. I will reply some day.</em></p>';
-		// clear $_POST so the form is not sticky
-		$_POST = [];
-	} else {
-		echo '<p style="font-weight: bold; color: #C00">Please fill out the form completely.</p>';
+
+	function spam_scrubber($value){
+		// list of bad values
+		$very_bad = ['to:', 'cc:', 'bcc:', 'content-type:', 'mime-version:',
+		'multipart-mixed:', 'content-transfer-encoding:'];
+		foreach($very_bad as $v){
+			if(stripos($value, $v) !== false) return '';
+		}
+		$value = str_replace(["\r", "\n", "%0a", "%0d"], ' ', $value);
+		return trim($value);
 	}
+
+	// clean the form data
+	$scrubbed = array_map('spam_scrubber', $_POST);
+
+	// form validation
+	if(!empty($scrubbed['name']) && !empty($scrubbed['email']) && !empty($scrubbed['comments'])){
+		$body = "Name: {$scrubbed['name']}\n\nComments: {$scrubbed['comments']}";
+		$body = wordwrap($body, 70);
+		mail('your_example@example.com', 'Contact Form Submission', $body, "From: {$scrubbed['email']}");
+		echo '<p><em>Thank you for contacting me. I will reply some day.</em></p>';
+		$scrubbed = [];
+	} else {
+		echo '<p style="font-weight: bold; color: #C000">Please fill out the form completely</p>';
+	}
+
 }
 ?>
 
@@ -34,11 +45,11 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 <form action="email.php" method="post">
 	<p>Name: <input type="text" name="name" size="30" maxlength="60" 
-	value="<?php if(isset($_POST['name'])) echo $_POST['name'] ?>"></p>
+	value="<?php if(isset($scrubbed['name'])) echo $scrubbed['name'] ?>"></p>
 	<p>Email: <input type="email" name="email" size="30" maxlength="80" 
-	value="<?php if(isset($_POST['email'])) echo $_POST['email'] ?>"></p>
+	value="<?php if(isset($scrubbed['email'])) echo $scrubbed['email'] ?>"></p>
 	<p>Comments: <textarea name="comments" rows="5" cols="30">
-		<?php if(isset($_POST['comments'])) echo $_POST['comments'] ?>
+		<?php if(isset($scrubbed['comments'])) echo $scrubbed['comments'] ?>
 	</textarea></p>
 	<p><input type="submit" name="submit" value="Send"></p>
 </form>
