3bc7777df98f0b27698266d855cf0e2e90cfc927
David Basil <info@basildavid.com>
2019-05-31 20:03:07 +0400

    edit calculator.js

--- ch15/js/calculator.js
+++ ch15/js/calculator.js
@@ -1,4 +1,6 @@
 $(function(){
+	// hide error messages
+	$('.errorMessage').hide();
 	// form handler
 	$('#calculator').submit(function(){
 		// initialize variables
@@ -6,27 +8,36 @@ $(function(){
 		// validate quantity	
 		if($('#quantity').val() > 0){
 			quantity = $('#quantity').val();
+			$('#quantityP').removeClass('error');
+			$('#quantityError').hide();
 		} else {
-			alert('Plase enter a valid quantity');
+			$('#quantityP').addClass('error');
+			$('#quantityError').show();
 		}
 		// validate price
 		if($('#price').val() > 0){
 			price = $('#price').val();
+			$('#priceP').removeClass('error');
+			$('#priceError').hide();
 		} else {
-			alert('Please enter a valid price');
+			$('#priceP').addClass('error');
+			$('#priceError').show();
 		}
 		// validate tax
 		if($('#tax').val() > 0){
 			tax = $('#tax').val();
+			$('#taxP').removeClass('error');
+			$('#taxError').hide();
 		} else {
-			alert('Please enter a valid tax');
+			$('#taxP').addClass('error');
+			$('#taxError').show();
 		}
 		// if data is set, perform calculations
 		if(quantity && price && tax){
 			total = quantity * price;
 			total += total * (tax/100);
 			// display results
-			alert('The total is $' + total);
+			$('#results').html('The total is <strong>$' + total + '</strong>');
 		}
 		return false;
 	})
